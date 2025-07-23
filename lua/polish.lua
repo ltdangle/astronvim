fbef16199a386eb338fb231b68c932a863ddd6cd
Evgeniy Fedorov <mailbox@z01d.net>
2025-07-23 14:30:14 +0300

    -- Allows C-w to escape terminal mode and act as a normal mode prefix

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,12 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+-- Allows C-w to escape terminal mode and act as a normal mode prefix
+vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], {
+  noremap = true,
+  silent = true,
+  desc = "Escape terminal mode for window commands",
+})
